acf563d0c79ab47cdbbfa7a465c2195cd40f5f6b
lizianegarcia <lizianegarciaa@gmail.com>
2023-04-18 22:14:25 -0300

    alterações

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
@@ -12,37 +12,47 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async(req, res) => {
+app.get("/", async(req, res) => {
     res.status(200).send({
-        message: 'Hello from Open AI in JS'
-    })
-})
+        message: "Say hi to Open AI",
+    });
+});
 
-app.post('/', async(req, res) => {
+app.post("/", async(req, res) => {
     try {
         const prompt = req.body.prompt;
 
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0, // Higher values means the model will take more risks.
-            max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-            top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-            frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-            presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
         });
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "X-Requested-With,content-type"
+        );
+        res.setHeader("Access-Control-Allow-Credentials", true);
 
         res.status(200).send({
-            bot: response.data.choices[0].text
+            bot: response.data.choices[0].text,
         });
-
     } catch (error) {
-        console.error(error)
-        res.status(500).send(error || 'Something went wrong');
+        console.error(error);
+        res.status(500).send(error || "Something went wrong");
     }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
+app.listen(5000, () =>
+    console.log("AI server started on http://localhost:5000")
+);
